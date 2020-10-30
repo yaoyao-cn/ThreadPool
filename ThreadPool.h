144f776e79efbb8d69e76d5313ba0285c24d3761
yao.zhao <yaoyao_cn@foxmail.com>
2020-10-30 09:19:37 +0800

    use modern c++

--- ThreadPool.h
+++ ThreadPool.h
@@ -10,32 +10,34 @@
 #include <future>
 #include <functional>
 #include <stdexcept>
+#include <type_traits>
 
-class ThreadPool {
+class ThreadPool
+{
+	ThreadPool(const ThreadPool&) = delete;
 public:
-    ThreadPool(size_t);
+    ThreadPool(size_t threads);
     template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) 
-        -> std::future<typename std::result_of<F(Args...)>::type>;
+    decltype(auto)  enqueue(F&& f, Args&&... args);
     ~ThreadPool();
+
 private:
     // need to keep track of threads so we can join them
-    std::vector< std::thread > workers;
+    std::vector< std::thread > m_workers;
     // the task queue
-    std::queue< std::function<void()> > tasks;
+    std::queue< std::function<void()> > m_tasks;
     
     // synchronization
-    std::mutex queue_mutex;
-    std::condition_variable condition;
-    bool stop;
+    std::mutex m_queue_mutex;
+    std::condition_variable m_condition;
+    bool m_stop = false;
 };
  
-// the constructor just launches some amount of workers
+// the constructor just launches some amount of m_workers
 inline ThreadPool::ThreadPool(size_t threads)
-    :   stop(false)
 {
     for(size_t i = 0;i<threads;++i)
-        workers.emplace_back(
+        m_workers.emplace_back(
             [this]
             {
                 for(;;)
@@ -43,13 +45,13 @@ inline ThreadPool::ThreadPool(size_t threads)
                     std::function<void()> task;
 
                     {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
-                        if(this->stop && this->tasks.empty())
+                        std::unique_lock<std::mutex> lock(this->m_queue_mutex);
+                        this->m_condition.wait(lock,
+                            [this]{ return this->m_stop || !this->m_tasks.empty(); });
+                        if(this->m_stop && this->m_tasks.empty())
                             return;
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
+                        task = std::move(this->m_tasks.front());
+                        this->m_tasks.pop();
                     }
 
                     task();
@@ -60,26 +62,25 @@ inline ThreadPool::ThreadPool(size_t threads)
 
 // add new work item to the pool
 template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) 
-    -> std::future<typename std::result_of<F(Args...)>::type>
+decltype(auto) ThreadPool::enqueue(F&& f, Args&&... args)
 {
-    using return_type = typename std::result_of<F(Args...)>::type;
+    using return_type = std::invoke_result_t<F, Args...>;
 
     auto task = std::make_shared< std::packaged_task<return_type()> >(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
         );
         
-    std::future<return_type> res = task->get_future();
+    auto res = task->get_future();
     {
-        std::unique_lock<std::mutex> lock(queue_mutex);
+        std::lock_guard<std::mutex> lock(m_queue_mutex);
 
         // don't allow enqueueing after stopping the pool
-        if(stop)
+        if(m_stop)
             throw std::runtime_error("enqueue on stopped ThreadPool");
 
-        tasks.emplace([task](){ (*task)(); });
+        m_tasks.emplace([task](){ (*task)(); });
     }
-    condition.notify_one();
+    m_condition.notify_one();
     return res;
 }
 
@@ -87,12 +88,15 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
 inline ThreadPool::~ThreadPool()
 {
     {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        stop = true;
+        std::lock_guard<std::mutex> lock(m_queue_mutex);
+        m_stop = true;
     }
-    condition.notify_all();
-    for(std::thread &worker: workers)
+    m_condition.notify_all();
+
+    for(auto& worker: m_workers)
+    {
         worker.join();
+    }
 }
 
 #endif
